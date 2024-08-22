36ce903461d96e4f60995b436fefd4f80ac3a014
NotE4sy <152352694+NotE4sy@users.noreply.github.com>
2024-08-22 15:20:12 +0800

    Release v1.0.0

--- bot.py
+++ bot.py
@@ -1,139 +1,107 @@
 import discord
-from discord.ext import commands
-import json
 import os
 import retrieveStats
-import profileHandler
 
+from discord.ext import commands
+from discord.ext.commands import CheckFailure
 from dotenv import load_dotenv
 
-# Set up intents (make sure to enable the message content intent in the Discord Developer Portal)
+#TODO: Make bot read from runner data doc so it auto updates, then make a command that reloads the bot
+#so endpoint isn't spammed
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Set up the bot with a command prefix and intents
-bot = commands.Bot(command_prefix='!!', intents=intents)
+bot = commands.Bot(command_prefix='!!', intents=intents, case_insensitive=True)
+ADMINROLES = {"Moderator", "Developer"}
+COMMANDSCHANNEL = int(os.getenv('DISCORD_COMMANDS_CHANNEL_ID'))
+TESTCOMMANDCHANNEL = int(os.getenv('DISCORD_COMMANDS_CHANNEL_ID_TEST_REMOVE_AFTER_BETA'))
+DATACHANNEL = int(os.getenv('DATA_CHANNEL_ID'))
+ALLOWED_CHANNEL_IDS = [COMMANDSCHANNEL, TESTCOMMANDCHANNEL]
 
-# Event: When the bot is ready
+parsed_data = None
+
+async def getProfileEmbed(profile):
+    url = f"https://minotar.net/helm/{profile[0]}/{128}"
+
+    embed = discord.Embed(title=f"{profile[0]}'s Stats",
+                          color=discord.Color.blurple())
+    embed.set_image(url=url)
+    embed.add_field(name="IGN", value=profile[0])
+    embed.add_field(name="PB", value=profile[2])
+    embed.add_field(name="Classic PB", value=profile[1])
+
+    return embed
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now running!')
     bot.loop.create_task(retrieveStats.retrieve_pace(bot))
 
-# Command: Profile management
+async def global_channel_check(ctx):
+    if ctx.channel.id not in ALLOWED_CHANNEL_IDS:
+        raise CheckFailure("This command cannot be used in this channel.")
+    return True
+
+bot.check(global_channel_check)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CheckFailure):
+        await ctx.send(error)
+    else:
+        await ctx.send(f"An error occurred: {str(error)}")
+
+@bot.command(name='commands')
+async def commands(ctx, *args):
+    await ctx.send('```'
+                   "!!profile stats <ign>\n"
+                   "!!commands\n"
+                   "!!classic\n"
+                   "!!man\n"
+                   '```')
+
+@bot.command(name='classic')
+async def classic(ctx, *args):
+    await ctx.send('MAN')
+
+@bot.command(name='test')
+async def test(ctx, *args):
+    await ctx.send('@Moderator')
+
+@bot.command(name='man')
+async def man(ctx, *args):
+    await ctx.send('CLASSIC')
+
 @bot.command(name='profile')
 async def profile(ctx, action, *args):
-    with open('res/profiles.json', 'r') as file:
-        profiles = json.load(file)
+    channel = bot.get_channel(DATACHANNEL)
+    if channel is None:
+        await ctx.send("Channel not found!")
+        return
 
-    if action == 'add':
+    async for msg in channel.history(limit=1):
+        lines = msg.content[3:-3].strip().split('\n')
+        parsed_data = [line.strip('<>').split(', ') for line in lines]
+    if action == 'stats':
         if len(args) < 1:
-            await ctx.send("Please provide a profile name.")
+            await ctx.send("Please provide an ign.")
             return
 
-        new_user = {
-            "profileName": args[0],
-            "ign": args[0],
-            "previousID": 0,
-            "completions": 0,
-            "pb": 8580000,
-            "classic pb": 8580000
-        }
+        ign = args[0]
 
-        if any(profile['ign'] == new_user['ign'] for profile in profiles):
-            await ctx.send(
-                f"{new_user['profileName']} is already in classicman!")
-        else:
-            profiles.append(new_user)
-            await ctx.send(
-                f"Successfully added {new_user['profileName']} to classicman!")
-
-        with open('res/profiles.json', 'w') as file:
-            json.dump(profiles, file, indent=4)
-
-    elif action == 'remove':
-        if len(args) < 1:
-            await ctx.send("Please provide a profile name.")
-            return
-
-        ign_to_remove = args[0]
-        to_be_removed = None
-
-        for profile in profiles:
-            if profile["ign"] == ign_to_remove:
-                to_be_removed = profile
-                break
-
-        if to_be_removed:
-            profiles.remove(to_be_removed)
-            await ctx.send(
-                f"Successfully removed {to_be_removed['profileName']} from classicman!"
-            )
-        else:
-            await ctx.send(f"{ign_to_remove} is not in classicman!")
-
-        with open('res/profiles.json', 'w') as file:
-            json.dump(profiles, file, indent=4)
-
-    elif action == 'stats':
-        if len(args) < 1:
-            await ctx.send("Please provide a profile name.")
-            return
-
-        profile_name = args[0]
-
-        for profile in profiles:
-            if profile['profileName'] == profile_name:
-                stats_embed = await profileHandler.getProfileEmbed(profile)
+        for profile in parsed_data:
+            if profile[0] == ign:
+                stats_embed = await getProfileEmbed(profile)
                 await ctx.send(embed=stats_embed)
                 break
         else:
-            await ctx.send(f"Profile {profile_name} not found.")
-
-    elif action == 'edit':
-        if len(args) < 3:
-            await ctx.send(
-                "Please provide a variable, new value, and profile name.")
-            return
-
-        variable, new_value, profile_name = args[0], args[1], args[2]
-        for profile in profiles:
-            if profile['profileName'] == profile_name:
-                if variable in profile:
-                    profile[variable] = new_value
-                    await ctx.send(
-                        f"Successfully updated {variable} for {profile_name}.")
-                else:
-                    await ctx.send(f"Variable {variable} not found in profile."
-                                   )
-                break
-        else:
-            await ctx.send(f"Profile {profile_name} not found.")
-
-        with open('res/profiles.json', 'w') as file:
-            json.dump(profiles, file, indent=4)
-
-    elif action == 'list':
-        profiles_sorted = sorted(profiles,
-                                 key=lambda x: x.get('classic pb', 0))
-        message_content = ''
-
-        for profile in profiles_sorted:
-            classic_time = retrieveStats.ms_convert(
-                profile['classic pb']
-            )
-            message_content += f"### Profile name: {profile['profileName']}\n"
-            message_content += f"Minecraft ign: {profile['ign']}\n"
-            message_content += f"Classic pb: {classic_time}\n\n"
-
-        await ctx.send(message_content)
-
+            await ctx.send(f"Runner {ign} not found.")
     else:
         await ctx.send("Invalid command params")
-
 
 # Run the bot with your token from the environment variable
 bot.run(TOKEN)
--- retrieveStats.py
+++ retrieveStats.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 import math
 import asyncio
 import discord
@@ -20,102 +19,126 @@ def ms_convert(millis):
 runs_url = 'https://paceman.gg/stats/api/getRecentRuns/'
 world_url = 'https://paceman.gg/stats/api/getWorld/'
 liveruns_url = "https://paceman.gg/api/ars/liveruns"
-
 ender_pearl_emote = "<:pearl:1265508441632407605>"
 blaze_rod_emote = "<:rod:1265507954157949061>"
 
+blinds = []
+strongholds = []
+enter_ends = []
+completions = []
+
+DATACHANNEL = int(os.getenv('DATA_CHANNEL_ID'))
+
 async def retrieve_pace(bot: commands.Bot):
     channel = bot.get_channel(int(os.getenv('DISCORD_CHANNEL_ID')))
-
-    # Open classicman profiles
-    with open('res/profiles.json', 'r') as file:
-        profiles = json.load(file)
-
+    parsed_data = None
     while True:
-        for profile in profiles:
-            # Getting response from API endpoint
-            response = requests.get(runs_url,
-                                    params={
-                                        'name': profile['ign'],
-                                        'hours': 1,
-                                        'limit': 1
-                                    })
+        dataChannel = bot.get_channel(DATACHANNEL)
 
+        async for msg in dataChannel.history(limit=1):
+            lines = msg.content[3:-3].strip().split('\n')
+            parsed_data = [line.strip('<>').split(', ') for line in lines]
+        
+        for profile in parsed_data:
+            response = requests.get(runs_url, params={'name': profile[0], 'hours': 1, 'limit': 1})
             if response.status_code == 200:
                 run = response.json()
                 if run:
-                    if 'first_portal' in run[0] and run[0][
-                            'first_portal'] is not None and profile[
-                                'previousID'] != run[0][
-                                    'id'] and 'bastion' in run[0] and run[0][
-                                        'bastion'] is None:
-                        profile['previousID'] = run[0]['id']
-                        enter_time = ms_convert(run[0]['first_portal'])
-
-                        with open('res/profiles.json', 'w') as file:
-                            json.dump(profiles, file, indent=4)
-
-                        if channel:
-                            rod_count = 0
-                            pearl_count = 0
-                            get_world_response = requests.get(
-                                url=f"{world_url}?worldId={run[0]['id']}")
-                            get_liveruns_response = requests.get(
-                                url=liveruns_url)
-                            live_msg = ""
-
-                            if get_world_response.status_code == 200 and get_liveruns_response.status_code == 200:
-                                world_data = get_world_response.json()
-                                liveruns_data = get_liveruns_response.json()
-
-                                for data in liveruns_data:
-                                    if data['nickname'] == profile['ign']:
-                                        rod_count = data['itemData'][
-                                            'estimatedCounts'][
-                                                'minecraft:blaze_rod']
-                                        pearl_count = data['itemData'][
-                                            'estimatedCounts'][
-                                                'minecraft:ender_pearl']
-                                        break
-
-                                if world_data['isLive'] is True and world_data[
-                                        'data']['vodId'] is not None:
-                                    live_msg = f"[{profile['profileName']}](<http://twitch.tv/{world_data['data']['vodId']}>)"
-                                else:
-                                    live_msg = f"Offline - {profile['profileName']} (ign: {profile['ign']})"
-
-                                await channel.send(
-                                    f"## {enter_time} - First Portal (Bastionless)\n\n"
-                                    f'{live_msg} <t:{int(time.time())}:R>\n'
-                                    f'{ender_pearl_emote} {pearl_count} {blaze_rod_emote} {rod_count}'
-                                )
-                        else:
-                            print("Channel not found.")
-
-                        # Handling completions
-                        if 'finish' in run[0] and run[0]['finish'] is not None:
-                            message = ""
-                            profile['completions'] += 1
-
-                            # Handling PBs
-                            if 'bastion' in run[0] and run[0][
-                                    'bastion'] is None:
-                                # Classic PBs
-                                if run[0]['finish'] < profile['classic pb']:
-                                    profile['classic pb'] = run[0]['finish']
-                                    message = f"{profile['profileName']} ({profile['ign']}) has just gotten a new classic pb of: {profile['classic pb']}!"
+                    if 'bastion' in run[0] and run[0]['bastion'] is None:
+                        if 'first_portal' in run[0] and run[0]['first_portal'] is not None and run[0]['id'] not in blinds:
+                            blinds.append(run[0]['id'])
+                            enter_time = ms_convert(run[0]['first_portal'])
+                            if channel:
+                                rod_count = 0
+                                pearl_count = 0
+                                get_world_response = requests.get(url=f"{world_url}?worldId={run[0]['id']}")
+                                get_liveruns_response = requests.get(url=liveruns_url)
+                                live_msg = ""
+                                if get_world_response.status_code == 200 and get_liveruns_response.status_code == 200:
+                                    world_data = get_world_response.json()
+                                    liveruns_data = get_liveruns_response.json()
+                                    for data in liveruns_data:
+                                        if data['nickname'] == profile[0]:
+                                            rod_count = data['itemData']['estimatedCounts']['minecraft:blaze_rod']
+                                            pearl_count = data['itemData']['estimatedCounts']['minecraft:ender_pearl']
+                                            break
+                                    if world_data['isLive'] is True and world_data['data']['vodId'] is not None:
+                                        live_msg = f"[{profile[0]}](<http://twitch.tv/{world_data['data']['twitch']}>)"
+                                    else:
+                                        live_msg = f"Offline - {profile[0]}"
+                                    await channel.send(f"## {enter_time} - First Portal (Bastionless)\n\n{live_msg}  <t:{int(time.time())}:R>\n{ender_pearl_emote} {pearl_count} {blaze_rod_emote} {rod_count}")
                             else:
-                                # Bastion PBs
-                                if run[0]['finish'] < profile['pb']:
-                                    profile['pb'] = run[0]['finish']
-                                    message = f"{profile['profileName']} ({profile['ign']}) has just gotten a new bastion pb of: {profile['pb']}!"
+                                print("Channel not found.")
 
-                            await channel.send(message)
-                    # Preventing too much spam and function crashing
+                        if 'stronghold' in run[0] and run[0]['stronghold'] is not None and run[0]['id'] not in strongholds:
+                            strongholds.append(run[0]['id'])
+                            enter_time = ms_convert(run[0]['stronghold'])
+                            if channel:
+                                get_world_response = requests.get(url=f"{world_url}?worldId={run[0]['id']}")
+                                get_liveruns_response = requests.get(url=liveruns_url)
+                                live_msg = ""
+                                if get_world_response.status_code == 200 and get_liveruns_response.status_code == 200:
+                                    world_data = get_world_response.json()
+                                    liveruns_data = get_liveruns_response.json()
+
+                                    if world_data['isLive'] is True and world_data['data']['vodId'] is not None:
+                                        live_msg = f"[{profile[0]}](<http://twitch.tv/{world_data['data']['twitch']}>)"
+                                    else:
+                                        live_msg = f"Offline - {profile[0]}"
+                                    await channel.send(f"## {enter_time} - Enter Stronghold (Bastionless)\n\n{live_msg}  <t:{int(time.time())}:R>\n")
+                            else:
+                                print("Channel not found.")
+
+                        if 'end' in run[0] and run[0]['end'] is not None and run[0]['id'] not in enter_ends:
+                            enter_ends.append(run[0]['id'])
+                            enter_time = ms_convert(run[0]['end'])
+                            if channel:
+                                rod_count = 0
+                                pearl_count = 0
+                                get_world_response = requests.get(url=f"{world_url}?worldId={run[0]['id']}")
+                                get_liveruns_response = requests.get(url=liveruns_url)
+                                live_msg = ""
+                                if get_world_response.status_code == 200 and get_liveruns_response.status_code == 200:
+                                    world_data = get_world_response.json()
+                                    liveruns_data = get_liveruns_response.json()
+                                
+                                    if world_data['isLive'] is True and world_data['data']['vodId'] is not None:
+                                        live_msg = f"[{profile[0]}](<http://twitch.tv/{world_data['data']['twitch']}>)"
+                                    else:
+                                        live_msg = f"Offline - {profile[0]}"
+                                    await channel.send(f"## {enter_time} - Enter End (Bastionless)\n\n{live_msg}  <t:{int(time.time())}:R>\n")
+                            else:
+                                print("Channel not found.")
+                            
+                    if 'finish' in run[0] and run[0]['finish'] is not None and run[0]['id'] not in completions:
+                        completions.append(run[0]['id'])
+                        
+                        get_world_response = requests.get(url=f"{world_url}?worldId={run[0]['id']}")
+                        get_liveruns_response = requests.get(url=liveruns_url)
+
+                        if get_world_response.status_code == 200 and get_liveruns_response.status_code == 200:
+                            world_data = get_world_response.json()
+                            liveruns_data = get_liveruns_response.json()
+
+                            if world_data['isLive'] is True and world_data['data']['vodId'] is not None:
+                                live_msg = f"[{profile[0]}](<http://twitch.tv/{world_data['data']['twitch']}>)"
+                            else:
+                                live_msg = f"Offline - {profile[0]}"
+                        
+                            if 'bastion' in run[0] and run[0]['bastion'] is None:
+                                await channel.send(f"## {ms_convert(run[0]['finish'])} - Finish (Bastionless)\n\n{live_msg}  <t:{int(time.time())}:R>\n")
+                                minutes, seconds = map(int, profile[1].split(':'))
+                                if run[0]['finish'] < (minutes * 60 + seconds) * 1000:
+                                    profile[1] = str(run[0]['finish'])
+                                    await channel.send(f"{profile[0]} has just gotten a new classic pb of: {profile[1]}!")
+                            else:
+                                minutes, seconds = map(int, profile[2].split(':'))
+                                if run[0]['finish'] < (minutes * 60 + seconds) * 1000:
+                                    profile[2] = str(run[0]['finish'])
+                                    
                     await asyncio.sleep(25)
-                else:
+                else: 
                     print("No recent runs")
-            else:
+            else: 
                 print(f"Failed to fetch data: {response.status_code}")
-
             await asyncio.sleep(1)
